a409686f0dfb1356a271f86a4c86e9856d6c182e
Shannon Bonet <shannonbonet@berkeley.edu>
2020-10-06 22:20:21 -0700

    Added material-ui/core and material-ui/icons. Created components folder with Learn & Resources. Happy Hacking :(

---
+++ src/components/Learn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Learn() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default Learn;
---
+++ src/components/Resources.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Resources() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default Resources;
