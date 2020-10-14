3ef517fb6e07c1762aa97c2eb45c412f58cf23b4
Kenneth Lien <kennethlien@berkeley.edu>
2020-10-13 21:11:40 -0700

    update

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,1 +1,33 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit';
+
+const initialState = { 
+  "lessons": {
+    0: {
+      "description": "",
+      "objectives": [],
+      "resourceIds": [],
+    }
+  },
+  "resources": {
+    0: {
+      "type": "",
+      "tags": [],
+      "data": {
+        "watchUrl": "",
+        "downloadUrl": "",
+        "fileSize": 0,   // File size in bytes
+        "duration": 0,   // Video duration in seconds
+      },
+      "isFinished": true,
+      "isCached": true,
+      "isFavorited": true,
+    }
+  },
+  "currentUser": {
+    "userId": "",
+    "favorited": [],
+    "downloaded": [],
+  }
+};
+
+
