05911ce4beb0487919e450c6bdad6bb227dfb705
Kenneth Lien <kennethlien@berkeley.edu>
2020-10-13 21:22:27 -0700

    more linter testing

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,9 +1,9 @@
 import { createReducer } from '@reduxjs/toolkit';
 
 const initialState = {
-  "lessons": {
+  lessons: {
     0: {
-      description: '',
+      'description': '',
       objectives: [],
       resourceIds: []
     }
