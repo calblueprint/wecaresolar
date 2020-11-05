eac6da36a860900f9cd6336bd4f665157296f2d7
Kevin Li <kevintli98@gmail.com>
2020-11-04 22:58:26 -0800

    Move service worker back to src/

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import * as serviceWorker from '../public/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store/reducers';
 
