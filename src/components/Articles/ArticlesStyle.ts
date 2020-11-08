eb4dc4ed373d56e926fa2963a775e97b1c19a359
Jared Keating <jkeating0002@berkeley.edu>
2020-11-07 18:21:36 -0800

    Style Pt. 2

--- src/components/Articles/ArticlesStyle.ts
+++ src/components/Articles/ArticlesStyle.ts
@@ -1,36 +1,36 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles({
-    root: {
-        padding: "11px",
-        width: "186px",
-        height: "102px",
-        border: "1px",
-        borderStyle: "solid",
-        borderColor: "#DEDEDE",
-        borderRadius: "10px",
-        marginLeft: "10px"
-    },
-    title: {
-        width: "133.69px",
-        height: "34.43px",
-        paddingLeft: "10px",
-        fontFamily: "Roboto",
-        fontStyle: "normal",
-        fontWeight: 700,
-        fontSize: "14px",
-        lineHeight: "16px",
-        color: "#8F8F8F"
-    },
-    description: {
-        width: "133px",
-        height: "10px",
-        paddingLeft: "10px",
-        fontFamily: "Roboto",
-        fontStyle: "normal",
-        fontWeight: 500,
-        fontSize: "10px",
-        lineHeight: "12px",
-        color: "#8F8F8F"
-    }
+  root: {
+    padding: '11px',
+    width: '186px',
+    height: '102px',
+    border: '1px',
+    borderStyle: 'solid',
+    borderColor: '#DEDEDE',
+    borderRadius: '10px',
+    marginLeft: '10px'
+  },
+  title: {
+    width: '133.69px',
+    height: '34.43px',
+    paddingLeft: '10px',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '14px',
+    lineHeight: '16px',
+    color: '#8F8F8F'
+  },
+  description: {
+    width: '133px',
+    height: '10px',
+    paddingLeft: '10px',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '10px',
+    lineHeight: '12px',
+    color: '#8F8F8F'
+  }
 });
