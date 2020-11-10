686c8660a82e7555f6a028e20555249b7b5b80c6
Jared Keating <jkeating0002@berkeley.edu>
2020-11-10 12:09:19 -0800

    Completed Button & Boolean Updating

--- src/pages/Favorites/Favorites.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import FavoriteList from '../../components/Favorites/FavoriteList';
-
-export default class Favorites extends Component {
-  render() {
-    return <FavoriteList />;
-  }
-}
