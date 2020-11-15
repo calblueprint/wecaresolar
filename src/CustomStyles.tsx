bb6a0d71dfa1c85b522db9105e570f00e5895cda
Shannon Bonet <shannonbonet@berkeley.edu>
2020-11-14 17:45:35 -0800

    waiting on design finalization

--- src/CustomStyles.tsx
+++ src/CustomStyles.tsx
@@ -1,27 +1,65 @@
 import React from 'react';
 import { createMuiTheme, Theme } from '@material-ui/core/styles';
-
+import { Palette } from "@material-ui/core/styles/createPalette";
 
 declare module '@material-ui/core/styles/createPalette' {
     interface Palette {
-        myCustomColor: Palette['primary']; 
+      main?: Palette['primary']
+      general?: Palette['primary']
+      suitcase?: Palette['primary']
+      lights?: Palette['primary']
+      fetaldoppler?: Palette['primary']
+      headlamp?: Palette['primary']
+      charger?: Palette['primary']
+      phonecharger?: Palette['primary']
     }
-}
-
+    interface PaletteOptions {
+      main?: PaletteOptions['primary']
+      general?: PaletteOptions['primary']
+      suitcase?: PaletteOptions['primary']
+      lights?: PaletteOptions['primary']
+      fetaldoppler?: PaletteOptions['primary']
+      headlamp?: PaletteOptions['primary']
+      charger?: PaletteOptions['primary']
+      phonecharger?: PaletteOptions['primary']
+    }
+  }
 
 
 const theme  = createMuiTheme({
     palette: {
-        myCustomColor: {
-            yellow: '#EFC477'
+        main: {
+          main: '#F6DBB7',
+          dark: '4F2A1D'
+        },
+        general: { 
+          main: '#F7F4F3',
+          dark: '#231F20'
         }, 
-        primary: {
-
+        suitcase: {
+          main: '#9FB8D8', 
+          dark: '#2A5375'
         }, 
-        secondary: {
-
+        lights: {
+          main: '#FCEBD5',
+          dark: '#DBB079'
         }, 
-
+        fetaldoppler: {
+          main: '#777CA6',
+          dark: '#4B5288'
+        },
+        headlamp: {
+          main: '#D49BA9',
+          dark: '#C0677D'
+        },
+        charger: {
+          main: '#DBE2E7',
+          dark: '#4D6A6D'
+        },
+        phonecharger: {
+          main: '#E4CDC7',
+          dark: '#BD9084'
+        },
     }
 })
 
--- src/components/SearchBar/SearchBarStyles.ts
+++ src/components/SearchBar/SearchBarStyles.ts
@@ -1,37 +1,37 @@
 import { createStyles, Theme, fade } from '@material-ui/core/styles';
 export const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            width: '100%',
-            position: "fixed",
-            top: 0,
-        },
-        search: {
-            position: 'relative',
-            borderRadius: 10,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            width: '100%',
-            margin: 10
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        inputRoot: {
-            color: 'inherit',
-            width: '100%'
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            width: '100%',
-            flexGrow: 1
-        }
-    });
+  createStyles({
+    root: {
+      flexGrow: 1,
+      width: '100%',
+      position: 'fixed',
+      top: 0
+    },
+    search: {
+      position: 'relative',
+      borderRadius: 10,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      width: '100%',
+      margin: 10
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%'
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      width: '100%',
+      flexGrow: 1
+    }
+  });
