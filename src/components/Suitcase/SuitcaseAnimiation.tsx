2a201a819586b4a8c72e343529ee8bf9bc5570e0
Shannon Bonet <shannonbonet@berkeley.edu>
2020-11-25 00:24:51 -0800

    need to solve 2 problems

--- src/components/Suitcase/SuitcaseAnimiation.tsx
+++ src/components/Suitcase/SuitcaseAnimiation.tsx
@@ -1,35 +1,55 @@
+import { SignalCellularNullTwoTone } from '@material-ui/icons';
 import React from 'react';
-import { ImageMapper, ImageMapperProps } from 'react-image-mapper2';
+import { ImageMapper, ImageMapperProps, Area, Map} from 'react-image-mapper2';
+import UserManual from "../Images/zoomed.jpg";
 
-var URL = "https://uc0b9469c9906f0abae862af9262.previews.dropboxusercontent.com/p/thumb/AA-hrrpSYB2lhSdcyXyVap7mS36duS-UjzeZT9_zskemHb_nWz2utrZFnFpl-Zm7fAxOC1htHcGy3vsxZFamlVMxZppXAeAF8P9VR-XVAuetOGqBf85H3O9M5ap2WQGDkqUOS8OQU5VQMIx1WQZ-tgwhcJk5dWiJkQwGs8779DSObWWBsxZO02HtO9ngSj2gcVS-r8fhNvWv8-YMOfF3vYbDQh4DlgMLjWwrld3tzMMlF-gGpMwFW1K6QHBhqZR1VvpeeJjQ9BOWgMIfZB-rFIPXSnWVtfD_f1MUB-qBRiEVnCqobKa6QgrjGSnuq6pcgD7-kGy7_JadQp_T9RD9wa3d4bQYhEFswnZOD74bP7KEcOPjgENPq3ptgjWslxPobuQ/p.png?fv_content=true&size_mode=5";
+var URL = "https://uc691fdfeef65b60eb7fb90adb3a.previews.dropboxusercontent.com/p/thumb/AA9GHu1AwFDMbmRdBDbgqn3dVT-DZ-QLbleObwjS8WboCpyZLDRfvDOlJrXDLQ07A17v8W8cqcnh07kKRkCsfIWxBAmmeRZW9AGH2sqSjJKRNd9EygWoYEWu0wPdhwWs2Iddz6Bpq6diRu0omwFSYOqi8MSwBi1wQiV2G_jja6ZbOeNzhkdmLP67dPiYKvZgMR9AMJzzepsyUtUlAeP3U_frv2uedYDIk2xJ-ja71SsVrCoF3Yz3PkZ7PI1lXvv66mi7Y8gdQdQLTzcnasN5uwoxD0R2F2z-HFF1qJSIpr6UsJwsGDGUSzBedsuM6sGyVCh88xLs23UmMFFt0WhQqiVOSflR7t7TR_EGzNsO7F9v0vd8xR5Vti9Xi_73KMfQ8OE/p.png?fv_content=true&size_mode=5"; 
 
-var MAP = {
-    name: "suitcase", 
-    areas: {
-        name: "fetus. HELP.", 
-        shape: "circle", 
-        coords: [25, 33, 27, 300, 128, 240, 128, 94],
-		preFillColor: "green",
+type MyState = { imgURLS: string[], imgINDEX: number, mapOfArea: Map};
+
+class SuitcaseAnimation extends React.Component<{}, MyState> {
+  constructor(props) {
+    super(props); 
+
+    const emptyCoords = null; 
+    const blue = "#6BADE8"; 
+    const radius = 10;
+
+    this.state={
+      imgURLS: ["https://uc691fdfeef65b60eb7fb90adb3a.previews.dropboxusercontent.com/p/thumb/AA9GHu1AwFDMbmRdBDbgqn3dVT-DZ-QLbleObwjS8WboCpyZLDRfvDOlJrXDLQ07A17v8W8cqcnh07kKRkCsfIWxBAmmeRZW9AGH2sqSjJKRNd9EygWoYEWu0wPdhwWs2Iddz6Bpq6diRu0omwFSYOqi8MSwBi1wQiV2G_jja6ZbOeNzhkdmLP67dPiYKvZgMR9AMJzzepsyUtUlAeP3U_frv2uedYDIk2xJ-ja71SsVrCoF3Yz3PkZ7PI1lXvv66mi7Y8gdQdQLTzcnasN5uwoxD0R2F2z-HFF1qJSIpr6UsJwsGDGUSzBedsuM6sGyVCh88xLs23UmMFFt0WhQqiVOSflR7t7TR_EGzNsO7F9v0vd8xR5Vti9Xi_73KMfQ8OE/p.png?fv_content=true&size_mode=5", 
+                UserManual], 
+      imgINDEX: 0, 
+      mapOfArea: {name: 'Suitcase', areas: [{ name: 'user manual', shape: 'circle', coords: [100, 230, radius], preFillColor: blue, strokeColor: blue }, 
+                                            { name: 'fetal', shape: 'circle', coords: [100, 100, radius], preFillColor: blue, strokeColor: blue }]}
+    }
+  }
+
+  // getInitialState() {
+	// 	return { hoveredArea: null, moveMsg: null };
+  // }
+  
+  enterArea(map) {
+    console.log(map.name);
+		this.setState({
+      imgINDEX: 1,
+    });
+  }
+        render() { 
+          return  <div>
+          <ImageMapper
+              active
+              src={this.state.imgURLS[this.state.imgINDEX]}
+              width={500}
+              map={this.state.mapOfArea}
+              // onLoad={() => this.getInitialState()} //bug: on initial load, buttons don't pop up
+              onClick={(map) => this.enterArea(map)} 
+           />
+      </div>
     }
 }
 
-const SuitcaseAnimation = () => {
-    return (
-        <div>
-            <ImageMapper
-                active
-                src={URL}
-                width={500}
-                map={{name: 'my-map',
-                areas: [
-                  { shape: 'poly', coords: [25,33,27,300,128,240,128,94], preFillColor: "green" },
-                  { shape: 'poly', coords: [219,118,220,210,283,210,284,119] },
-                  { shape: 'poly', coords: [381,241,383,94,462,53,457,282] },
-                  { shape: 'poly', coords: [245,285,290,285,274,239,249,238] },
-                ]}}
-			 />
-        </div>
-    )
-}
-
 export default SuitcaseAnimation; 
+
+
+//cannot do state inside functional components
+//can use hooks in functional components
