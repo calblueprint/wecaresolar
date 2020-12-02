595261cb1a653cdf851442317b54ca0d9f806534
Susan Lin <46883811+susan-lin@users.noreply.github.com>
2020-12-01 20:47:48 -0800

    Update VideoFunctions.tsx

--- src/components/Video/VideoFunctions.tsx
+++ src/components/Video/VideoFunctions.tsx
@@ -1,5 +1,4 @@
 export function getId(url) {
-  console.log("HELLO")
   const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
   return (match && match[2].length === 11) ? match[2] : null;
