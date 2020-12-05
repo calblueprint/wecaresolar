3368341756030fd02ec66f2b472717f32faaf128
Joelene Latief <joelenelatief@berkeley.edu>
2020-12-05 12:25:24 -0800

    color change

--- src/components/TabBar/TabBarStyles.tsx
+++ src/components/TabBar/TabBarStyles.tsx
@@ -1,17 +1,22 @@
 import { createStyles, Theme } from '@material-ui/core/styles';
 
 export const styles = (theme: Theme) =>
-    createStyles({
-        navbar: {
-            position: 'fixed',
-            width: '100%',
-            bottom: 0,
-        },
-        text: {
-            font: 'Roboto',
-            color: '#4F2A1D',
-            fontSize: '12px',
-            fontWeight: 700
-        }
+  createStyles({
+    navbar: {
+      position: 'fixed',
+      width: '100%',
+      bottom: 0
+    },
+    text: {
+      font: 'Roboto',
+      color: theme.palette.brown.main,
+      fontSize: '12px',
+      fontWeight: 700,
+      '&$selected': {
+        color: theme.palette.primary.main
+      }
+    },
+    selected: {
+      color: theme.palette.brown.main
     }
-    );
+  });
