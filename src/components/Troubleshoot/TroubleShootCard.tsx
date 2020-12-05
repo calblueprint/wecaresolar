44586aaa4c0a60c90e6fd7d6c5a19f837df7f1a8
Kenneth Lien <kennethlien@berkeley.edu>
2020-12-05 12:41:31 -0800

    more troubleshoot

--- src/components/Troubleshoot/TroubleShootCard.tsx
+++ src/components/Troubleshoot/TroubleShootCard.tsx
@@ -6,43 +6,72 @@ import { RootState } from '../../store/reducers';
 import { styles } from './TroubleShootStyles';
 import Button from '@material-ui/core/Button';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
 type TroubleShootProps = {
-  helpId: string,
-  classes: any,
-}
+  helpId: string;
+  classes: any;
+};
 
 const TroubleShootCard = ({ helpId, classes }: TroubleShootProps) => {
-
   const troubleshoot = useSelector((state: RootState) => state.troubleshoot);
   const help = troubleshoot[helpId];
+  const root = helpId === 'Root';
+  console.log(helpId)
+  const createRootOptions = (options) => {
+    return (
+      <div className={classes.optionContainer}>
+        {Object.keys(options).map((option) => {
+          return (
+            <Link
+              to={'/Troubleshoot/' + options[option]}
+              style={{ textDecoration: 'none' }}
+            >
+              <div className={classes.optionCard}>
+                <div className={classes.optionImage}> </div>
+                <div className={classes.optionText}>{option}</div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  };
+
   const createOptions = (options) => {
     return (
       <div>
         {Object.keys(options).map((option) => {
-          if (option == "error_code") {
+          if (option == 'error_code') {
           } else {
             let buttonType = `${classes.button}`;
-            if (option == "yes") {
-              buttonType = `${classes.button} ${classes.yes}`
-            } else if (option == "no") {
-              buttonType = `${classes.button} ${classes.no}`
+            if (option == 'yes') {
+              buttonType = `${classes.button} ${classes.yes}`;
+            } else if (option == 'no') {
+              buttonType = `${classes.button} ${classes.no}`;
             }
-            return <Link to={'/Troubleshoot/' + options[option]} style={{ textDecoration: 'none' }}>
-              <Button className={buttonType}>
-                {option}
-              </Button>
-            </Link>
+            return (
+              <Link
+                to={'/Troubleshoot/' + options[option]}
+                style={{ textDecoration: 'none' }}
+              >
+                <Button className={buttonType}>{option}</Button>
+              </Link>
+            );
           }
         })}
-        {help['parent'] ?
-          <Link to={'/Troubleshoot/' + help['parent']} style={{ textDecoration: 'none' }}>
+        {help['parent'] ? (
+          <Link
+            to={'/Troubleshoot/' + help['parent']}
+            style={{ textDecoration: 'none' }}
+          >
             <Button className={`${classes.button}`}>Back</Button>
-          </Link> :
+          </Link>
+        ) : (
           <span />
-        }
-      </div >
-    )
-  }
+        )}
+      </div>
+    );
+  };
   return (
     <div className={classes.card}>
       <div className={classes.header}>
@@ -51,9 +80,10 @@ const TroubleShootCard = ({ helpId, classes }: TroubleShootProps) => {
       </div>
       <h3>{help.title}</h3>
       <p>{help.description}</p>
-      {createOptions(help.options)}
+      {root && createRootOptions(help.options)}
+      {!root && createOptions(help.options)}
     </div>
   );
-}
+};
 
 export default withStyles(styles)(TroubleShootCard);
