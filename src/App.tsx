9a80b6ee90650387b6a2986b157006b11f579033
Joelene Latief <joelenelatief@berkeley.edu>
2020-12-05 12:47:32 -0800

    removed the extra router

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import theme from './CustomStyles';
 const useStyles = makeStyles({
   root: {
     color: theme.palette.background.default,
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: theme.palette.background.default
   }
 });
 
@@ -26,56 +26,42 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
           <div>
             <Route component={SearchBar} />
           </div>
           <div>
             <Switch>
               <Route exact path="/suitcase" component={Suitcase} />
-              <Route path={"/suitcase/:topicId"}
-                render={props =>
+              <Route
+                path={'/suitcase/:topicId'}
+                render={(props) => (
                   <TopicViews topicId={props.match.params.topicId} />
-                } />
+                )}
+              />
               <Route exact path="/favorites" component={Favorites} />
               <Route exact path="/guides" component={Guides} />
-              <Route path={"/guides/:lessonId"}
-                render={props =>
+              <Route
+                path={'/guides/:lessonId'}
+                render={(props) => (
                   <PlaylistPage lessonId={props.match.params.lessonId} />
-                } />
+                )}
+              />
               <Route exact path="/troubleshoot" component={Troubleshoot} />
-              <Route path={"/Troubleshoot/:helpId"}
-                render={props =>
+              <Route
+                path={'/Troubleshoot/:helpId'}
+                render={(props) => (
                   <TroubleShootCard helpId={props.match.params.helpId} />
-                } />
+                )}
+              />
             </Switch>
           </div>
           <div>
             <TabBar />
-          </div >
+          </div>
         </div>
         <TabBar />
       </Router>
-    <Router>
-      <div className={classes.root}>
-        <Route component={SearchBar} />
-        <Switch>
-          <Route exact path="/suitcase" component={Suitcase} />
-          <Route path={"/suitcase/:topicId"}
-            render={props =>
-              <TopicViews topicId={props.match.params.topicId} />
-            } />
-          <Route exact path="/favorites" component={Favorites} />
-          <Route exact path="/guides" component={Guides} />
-          <Route path={"/guides/:lessonId"}
-            render={props =>
-              <PlaylistPage lessonId={props.match.params.lessonId} />
-            } />
-          <Route path="/troubleshoot" component={Troubleshoot} />
-        </Switch>
-      </div>
-      <TabBar />
-    </Router>
     </ThemeProvider>
   );
 }
