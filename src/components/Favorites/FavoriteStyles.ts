3807c66c5b1390c80b7d3562085e1d8955941e18
Susan Lin <susan.lin@berkeley.edu>
2020-12-06 09:45:57 -0800

    styling start

--- src/components/Favorites/FavoriteStyles.ts
+++ src/components/Favorites/FavoriteStyles.ts
@@ -6,7 +6,46 @@ export const styles = (theme: Theme) =>
       color: theme.palette.brown.main,
       backgroundColor: 'Transparent',
       border: 'none'
+    },
+    page: {
+      marginTop: "10%",
+      margin: "3%",
+      overflow: "scroll",
+    },
+    header: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingRight: "5%"
+    },
+    title: {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+      color: theme.palette.brown.main
+    },
+    // root: {
+    //   backgroundColor: 'white',
+    //   border: 'none',
+    //   borderColor: 'white',
+    //   borderRadius: '55%',
+    //   boxShadow:
+    //     '0.896084px 2.38956px 2.98695px 0.896084px rgba(218, 207, 207, 0.5)',
 
-    }
+    // },
+    // select: {
+    //   '&:before': {
+    //     border: 'none',
+    //   },
+    //   '&:after': {
+    //     border: 'none',
+    //   },
+    //   '&:hover': {
+    //     border: 'none',
+    //     borderColor: 'white',
+    //   },
+    // },
+    // outlined: {
+    //   border: 'none',
+    // }
   }
-);
+  );
--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,8 +1,51 @@
-import React, { Component } from 'react';
-import FavoriteList from '../../components/Favorites/FavoriteList';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import StandardCard from '../../components/Cards/StandardCard';
+import { selectFavoritedResources } from '../../store/resourcesSlice';
+import { withStyles } from "@material-ui/core/styles";
+import { styles } from '../../components/Favorites/FavoriteStyles';
+import { Link } from 'react-router-dom';
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 
-export default class Favorites extends Component {
-  render() {
-    return <FavoriteList />;
-  }
+function Favorites({ match, classes }) {
+  const favResources = useSelector(selectFavoritedResources);
+  const [resType, setresType] = useState('all');
+  const [Topic, setTopic] = useState('');
+
+  const handleResChange = (event) => {
+    setresType(event.target.value);
+  };
+  const handleTopicChange = (event) => {
+    setTopic(event.target.value);
+  };
+  return (
+    <div className={classes.page}>
+      <div className={classes.header}>
+        <div className={classes.title}>Favorites</div>
+        <FormControl >
+          <Select value={resType}
+            className={classes.select}
+            classes={{ root: classes.root }}
+            onChange={handleResChange}>
+            <MenuItem value={'all'}>All</MenuItem>
+            <MenuItem value={10}>Videos</MenuItem>
+            <MenuItem value={20}>Articles</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      {Object.keys(favResources).map((resource: any) => (
+        <Link style={{ textDecoration: 'none', margin: '1%' }} to={`resources/${resource}`}>
+          <StandardCard key={resource} resource={favResources[resource]} resourceID={resource} />
+        </Link>
+      ))
+      }
+    </div >
+  );
 }
+
+export default withStyles(styles)(Favorites);
