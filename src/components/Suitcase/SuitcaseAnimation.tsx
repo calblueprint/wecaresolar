8e7146272fc197b915d271743268556102dd47cf
Shannon Bonet <shannonbonet@berkeley.edu>
2021-03-06 17:51:56 -0800

    zoom in and zoom out functionality

--- src/components/Suitcase/SuitcaseAnimation.tsx
+++ src/components/Suitcase/SuitcaseAnimation.tsx
@@ -5,50 +5,82 @@ type SuitcaseProps = {
     match 
 }
 
-//functional component, access to state 
 const SuitcaseAnimation = (props: SuitcaseProps) => {
-    const [clicked, setClicked] = useState(false); //click initially false
-    const [sx, setSx] = useState(0); //set x coordinate
-    const [sy, setSy] = useState(0); //set y coordinate 
-    const [sWidth, setSWidth] = useState(300); //sets cropped width 
-    const [sHeight, setSHeight] = useState(300); //sets cropped height 
-    const width = window.innerWidth; 
-    const height = window.innerHeight; 
+    const [clicked, setClicked] = useState(false); //use for zoom in/out later
 
-    const canvasRef = useRef<HTMLCanvasElement | null>(null); //create reference to canvas
-    const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null); //creates context for editing canvas
-    const image = new Image(); //creates an ImageData object. 
+    /* Create an ImageData object */ 
+    const image = new Image(); 
     image.src = Suitcase;
 
-    //triggers when state changes 
+    /* Set dimensions of interactive map: 
+        cWidth, cHeight: canvas dimensions; set to user's window dimensions 
+        sWidth, sHeight: scaling dimensions; set to dimensions of source image 
+        sx, sy: starting coordinates of drawn image
+    */ 
+    const cWidth = window.innerWidth; 
+    const cHeight = window.innerHeight; 
+    const [sWidth, setSWidth] = useState(image.width); 
+    const [sHeight, setSHeight] = useState(image.height); 
+    const [sx, setSx] = useState(0);
+    const [sy, setSy] = useState(0);
+
+    /* Create a reference to null Canvas object. 
+        Then, create a context reference for drawing on Canvas. 
+    */ 
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+    /* Hook triggers on changes to source image values. 
+        Drawing Image: 
+            1. Retrieve a drawing context from our context reference. 
+            2. Create a context for drawing. 
+            3. Clear Canvas before drawing new image. 
+            4. drawImage takes in (image,     <-- ImageData object 
+                    sx, sy, sWidth, sHeight   <-- Starting context, selects starting coordinate & scaling dimensions (Optional)
+                    dx, dy, dWidth, dHeight)  <-- Destination context, used to scale source to destination. 
+    */ 
     useEffect(() => {
-        setSx(2); //use to automatically 
-        if (canvasRef.current) { //if reference object exists 
+        if (canvasRef.current) {
             canvasCtxRef.current = canvasRef.current.getContext("2d");
             const ctx = canvasCtxRef.current;
-            ctx!.drawImage(image, 0, 0, width, height); //change this line after completing math function
-            console.log(canvasRef.current.width); 
+            ctx!.clearRect(0, 0, cWidth, cHeight); 
+            ctx!.drawImage(image, sx, sy, sWidth, sHeight, 0, 0, cWidth, cHeight); 
         }
-        console.log(canvasRef); 
     }, [image]); 
     
-    
-    //sets new sx, sy coordinate based on dot coordinate
-    //indirectly calls useEffect 
+    /* Sets new sx, sy, sWidth, sHeight values based on input x and y */ 
     function zoomIn(x, y) {
-        return; 
+        const zoomWidth = image.width / 3; 
+        const zoomHeight = image.height / 3; 
+        const top_x = zoomWidth; 
+        const top_y = zoomHeight; 
+        setSx(top_x); 
+        setSy(top_y); 
+        setSWidth(zoomWidth); 
+        setSHeight(zoomHeight); 
     }
+
+    /* Resets sx, sy, sWidth, sHeight to original values */ 
+    function zoomOut() {
+        setSx(0); 
+        setSy(0); 
+        setSWidth(image.width); 
+        setSHeight(image.height); 
+    }
+
+    // console.log("xcord:", top_x, "ycord:", top_y, "zoomW:", zoomWidth, "zoomH:", zoomHeight)
+    // console.log("image w:", image.width, "image h:", image.height, "canvas w:", eWidth, "canvas h:", eHeight); 
 
     return (
         <div>
-            <button onClick={() => zoomIn(200, 200)}> ZOOM IN </button>
+            <button onClick={() => zoomIn(475, 475)}> ZOOM IN </button>
+            <button onClick={() => zoomOut()}>ZOOM OUT </button>
             <canvas 
                 ref={canvasRef}
-                width={width}
-                height={height}
+                width={cWidth}
+                height={cHeight}
             ></canvas>
         </div>
     )
 }
-
 export default SuitcaseAnimation; 
