d20ffb395dfea0fcd6b4fb9cab75d5ead5076a9f
Joyce Guo <yj.guo@berkeley.edu>
2021-03-28 15:52:56 -0700

    standard card styling format fixes

--- src/components/CardComponents/CardButtonStyles.ts
+++ src/components/CardComponents/CardButtonStyles.ts
@@ -1,16 +1,16 @@
-import { createStyles, Theme} from '@material-ui/core/styles';
+import { createStyles, Theme } from '@material-ui/core/styles';
 import { Palette } from '@material-ui/icons';
-export const styles = (theme: Theme) => 
-    createStyles({
-        favorited: {
-            fill: 'none'
-        },
-        checkmark: {
-            // background: 'none',
-            // border: 'none',
-            // borderRadius: '100%',
-            // display: 'flex',
-            // justifyContent: 'center',
-            // alignItems: 'center'
-        }
-    });
+export const styles = (theme: Theme) =>
+  createStyles({
+    favorited: {
+      fill: 'none'
+    },
+    checkmark: {
+      // background: 'none',
+      // border: 'none',
+      // borderRadius: '100%',
+      // display: 'flex',
+      // justifyContent: 'center',
+      // alignItems: 'center'
+    }
+  });
