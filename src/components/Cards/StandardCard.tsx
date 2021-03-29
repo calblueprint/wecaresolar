6d310b2de244ac87a5ec31efd5858ea83d252d90
Susan Lin <46883811+susan-lin@users.noreply.github.com>
2021-03-28 18:07:34 -0700

    Update StandardCard.tsx

--- src/components/Cards/StandardCard.tsx
+++ src/components/Cards/StandardCard.tsx
@@ -11,7 +11,7 @@ interface StandardCardProps {
   resourceID: number;
   classes: any;
   completeCheck: boolean;
-  collapsed?: boolean;
+  collapsed: boolean;
 }
 
 const StandardCard = (props: StandardCardProps) => {
