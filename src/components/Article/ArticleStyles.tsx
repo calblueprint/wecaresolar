60f1e6a3517bf8eb789d7e098752393b5c26a5b9
Jared Keating <jaredkeating@Jareds-MacBook-Pro.local>
2021-04-10 05:00:14 -0700

    Created Section View Component

---
+++ src/components/Article/ArticleStyles.tsx
@@ -0,0 +1,4 @@
+import { createStyles, Theme, } from '@material-ui/core/styles';
+export const styles = (theme: Theme) =>
+  createStyles({
+});
---
+++ src/components/Article/Section.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import { styles } from './SectionStyles';
+
+type SectionProps = {
+    section: any,
+    classes?: any,
+}
+
+const Section = ({ section, classes}: SectionProps): typeof Section => {
+    return (
+        <div className={classes.labelBox}>
+        <text className={classes.labelText}>{section.label}</text>
+        <div className={classes.title}>
+            {section.title}
+        </div>
+        <div>
+            <img className={classes.image} src={section.imageUrl}></img>
+        </div>
+        <div>
+            <text className={classes.text}>{section.text}</text>
+        </div>
+    </div>
+    );
+}
+
+export default withStyles(styles)(Section);
