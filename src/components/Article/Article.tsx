92056571bf52943dc55959f1529e85fb8353de50
Jared Keating <jaredkeating@Jareds-MacBook-Pro.local>
2021-04-17 15:46:38 -0700

    created and some styling for article page

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/reducers';
 import { isArticle } from '../../store/resourcesSlice';
+import ProgressBar from '../Guides/ProgressBar';
 import { styles } from './ArticleStyles';
-import Section from './Section';
+import SectionCard from './Section';
 
 type ArticleProps = {
   resId: string,
@@ -12,12 +13,40 @@ type ArticleProps = {
 }
 
 const Article = ({ resId, classes }: ArticleProps): typeof Article => {
-  const resources = useSelector((state: RootState) => state.resources)
-  const article = resources[resId]
-  const sections = useSelector((state: RootState) => state.sections)
+    const resources = useSelector((state: RootState) => state.resources)
+    const article = resources[resId]
+    const sections = useSelector((state: RootState) => state.sections)
+    const calcProgress = () => {
+        return 25
+    }
+    
+    if (isArticle(article.data)) {
+        const articleSections = article.data.sections
+        return (
+            <div>
+                <div className={classes.title}>
+                    <Typography variant="h1">{article.title}</Typography>
+                </div>
+                <div className={classes.progressBar}>
+                    {ProgressBar(calcProgress())}
+                    <Typography variant="body2" className={classes.progressText}>% Complete</Typography>
+                </div>
+                <div className = {classes.sectionView}>
+                    {articleSections.map((key: any) => (
+                        <div className={classes.section}>
+                            <SectionCard section={sections[key]}></SectionCard>
+                        </div>)
+                    )}
+                </div>
+            </div>
+            )
+}
 
-  if (isArticle(article.data)) {
-    const articleSections = article.data.sections
+
+//
+
+/** 
+>>>>>>> Stashed changes
     return (
       <div>
         <div className={classes.title}>
--- src/components/Article/Section.tsx
+++ src/components/Article/Section.tsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
 import { styles } from './SectionStyles';
+import { Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import { Section } from '../../store/sectionsSlice';
 
 type SectionProps = {
-  section: any,
+  section: Section,
   classes?: any,
 }
 
-const Section = ({ section, classes }: SectionProps): typeof Section => {
-  return (
-    <div className={classes.section}>
-      <div className={classes.labelBox}>
-        <text className={classes.labelText}>{section.label}</text>
-      </div>
-      <div className={classes.title}>
-        {section.title}
-      </div>
-      <div>
-        <img className={classes.image} src={section.imageUrl}></img>
-      </div>
-      <div>
-        <text className={classes.text}>{section.text}</text>
-      </div>
-    </div>
-  );
+const SectionCard = ({ section, classes}: SectionProps): typeof SectionCard => {
+    if (section.imageUrl == null) {
+        return (
+            <Card className={classes.section}>
+                <div>
+                    <div className={classes.labelBox}>
+                        <text className={classes.labelText}>{section.label}</text>
+                    </div>
+                    <div className={classes.title}>
+                            <Typography variant="h2">{section.title}</Typography>
+                    </div>
+                    <div className={classes.text}>
+                        <Typography variant="body1">{section.text}</Typography>
+                    </div> 
+                </div>
+            </Card>
+        );
+    } else {
+        return (
+            <Card className={classes.section}>
+                <div>
+                    <div className={classes.labelBox}>
+                        <text className={classes.labelText}>{section.label}</text>
+                    </div>
+                    <div className={classes.title}>
+                            <Typography variant="h2">{section.title}</Typography>
+                    </div>
+                    <div className={classes.imageDiv}>
+                        <img className={classes.image} src ={section.imageUrl}></img>
+                    </div>
+                    <div className={classes.textImage}>
+                        <Typography variant="body1">{section.text}</Typography>
+                    </div> 
+                </div>
+            </Card>
+        );
+    }
 }
 
-export default withStyles(styles)(Section);
+export default withStyles(styles)(SectionCard);
