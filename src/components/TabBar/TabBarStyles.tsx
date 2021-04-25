1188bfe92025dbf9e54b7f996df74e33be8ebc7f
Shannon Bonet <shannonbonet@berkeley.edu>
2021-04-24 17:39:21 -0700

    fixed opacity bug

--- src/components/TabBar/TabBarStyles.tsx
+++ src/components/TabBar/TabBarStyles.tsx
@@ -5,20 +5,18 @@ export const styles = (theme: Theme) =>
     root: {
       minWidth: 60,
       maxWidth: '100%',
-      opacity: 0.65,
-      color: theme.palette.black.main,
+      color: theme.palette.navgrey.main,
       padding: '0%',
       '&$selected': {
         width: '100%',
         color: theme.palette.yellow2.main,
         borderTop: 'solid',
-        opacity: 1
       }
     },
     fabRoot: {
       display: 'flex',
       flexDirection: 'column',
-      color: theme.palette.black.main,
+      color: theme.palette.navgrey.main,
       backgroundColor: theme.palette.white.main,
       position: 'fixed',
       bottom: '2.5vh',
@@ -28,7 +26,6 @@ export const styles = (theme: Theme) =>
       '&:hover': {
         color: theme.palette.yellow2.main,
         backgroundColor: theme.palette.white.main,
-        opacity: 1,
         border: '3px solid',
         borderColor: theme.palette.yellow2.main
       }
